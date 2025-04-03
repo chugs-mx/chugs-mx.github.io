a3842893ca0c2887c9ed035500601f8458f4f717
rosaainz <rosagsainzz@gmail.com>
2025-04-03 00:04:13 -0600

    sidebar and nav bar desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import Topbar from "@/components/Topbar";
 
 export default function Home() {
   return (
-    <><Link href="/" className="flex justify-start ml-6 mt-6">
-      <Image src={"/chug-logo.webp"} alt='Logo' width={220} height={180}></Image>
-      <ul className='hidden h-full gap-12 lg:flex'></ul>
-    </Link><Navbar></Navbar></>
+    <>
+    <Topbar></Topbar>
+    <Navbar></Navbar></>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,50 +8,56 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-   
+    <>
+    <button 
+        onClick={() => setIsOpen(!isOpen)} 
+        className="fixed top-4 left-0 z-50 md:hidden bg-primary p-3 rounded-br-lg shadow-black/30 shadow-xl"
+      >
+        <Image src={"/nav/burguerBar_icon.webp"} alt="Menu" width={40} height={40}  />
+    </button>
 
-      <nav className='fixed top-35 left-0 h-full bg-primary shadow-2xl flex flex-col items-start py-6 px-6 rounded-tr-lg transition-all duration-300 ${isOpen ? "w-20" : "w-40"}'>
+      <nav  className={`fixed top-15 left-0 h-full bg-primary  shadow-black/50 shadow-xl flex flex-col items-start py-6 px-6 rounded-tr-lg transition-all duration-300 
+          ${isOpen ? "w-50" : "w-0 hidden "} md:w-25 md:flex md:top-35`}>
+          
           <div className='flex flex-col gap-6'>
-          <button onClick={() => setIsOpen(!isOpen)}>
-            <Image src={"/home/burguerBar_icon.webp"} alt="Menu" width={30} height={30} />
-          </button>
-
+         
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/home_icon.webp"} alt="Home" width={30} height={30}/>
-              {isOpen && <span className='text-primary-foreground'>Home</span>}
+              <Image src={"/nav/home_icon.webp"} alt="Home" width={30} height={30}/>
+              {isOpen && <span className='text-primary-foreground md:hidden'>Home</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/order_icon.webp"} alt="Órdenes" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Órdenes</span>}
+              <Image src={"/nav/order_icon.webp"} alt="Órdenes" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Órdenes</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/tables_icon.webp"} alt="Órdenes" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Mesas</span>}
+              <Image src={"/nav/tables_icon.webp"} alt="Órdenes" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Mesas</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/add_products_icon.webp"} alt="Agregar" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Agregar</span>}
+              <Image src={"/nav/add_products_icon.webp"} alt="Agregar" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Agregar</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/inventory_icon.webp"} alt="Inventario" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Inventario</span>}
+              <Image src={"/nav/inventory_icon.webp"} alt="Inventario" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Inventario</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/metrics_icon.webp"} alt="Métricas" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Métricas</span>}
+              <Image src={"/nav/metrics_icon.webp"} alt="Métricas" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Métricas</span>}
             </div>
 
             <div className={`flex items-center gap-2 cursor-pointer ${isOpen ? "border-b border-primary-foreground pb-2" : ""}`}>
-              <Image src={"/home/settings_icon.webp"} alt="Configuración" width={30} height={30} />
-              {isOpen && <span className='text-primary-foreground'>Configuración</span>}
+              <Image src={"/nav/settings_icon.webp"} alt="Configuración" width={30} height={30} />
+              {isOpen && <span className='text-primary-foreground md:hidden'>Configuración</span>}
             </div>
           </div>
       </nav>
+    </>
   )
 }
 
