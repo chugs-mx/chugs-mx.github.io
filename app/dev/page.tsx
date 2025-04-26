25d65bf13f7c982967524d9a592ceafb3ed0bcde
erickdjm <erick.dejesus1@alumno.buap.mx>
2025-04-25 19:27:19 -0600

    Update dev page to fetch and display inventory data
    Replaced user authentication logic with a fetch call to retrieve inventory data from a local API. The response is processed to display inventory item names dynamically on the page. Removed unused authentication-related code and updated the UI layout accordingly.

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,20 +1,24 @@
-import {auth} from "@/auth";
 
-export default async function (){
-    const session = await auth();
-    if (!session?.user) {
-        return (
-            <div>
-                <h1>Not logged in</h1>
-            </div>
-        )
 
-    }
+export default async function () {
+    const data = await fetch("http://localhost:8080/inventories").then((res) => res.json()).catch((err) => {
+        console.log(err)
+    })
+
+
+    const arr = data.content
+    const names = arr.map((i:any)=> i.name);
+
     return (
         <>
-            {`Bienvenido: ${session.user.name}`}
-            <div className={"flex justify-center items-center "}>
-                {JSON.stringify(session)}
+            <div className="flex justify-center items-center min-h-screen flex-col" >
+                <h1 className="text-8xl">Hola Dev!!!</h1>
+
+                {names.map((name:string) => (
+                    <div key={name}>
+                        <h1 className="text-4xl">{name}</h1>
+                    </div>
+                ))}
             </div>
         </>
     )
