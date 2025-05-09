be06d100e2f8b231aaf927f291fb345844a252d1
rosaainz <rosagsainzz@gmail.com>
2025-05-09 17:31:41 -0600

    add correct import columns

--- components/product/columns.tsx
+++ components/product/columns.tsx
@@ -1,43 +1,43 @@
-'use client'
+"use client";
 
-import { translateCategory, translateMeasureUnit } from "@/components/inventories/translations";
+import { translateCategory } from "@/components/product/translations";
 import { Column } from "@/components/data-table";
-import { Product } from "@/types/product_template";
+import { Product } from "@/types/Product";
 import { Pencil, StickyNote } from "lucide-react";
 import { Trash2 } from "lucide-react";
 
 export const columns: Column<Product>[] = [
-        {
-          label: "Nombre",
-          field: "name"
-        },
-        {
-            label: "Descripción",
-            field: "description"
-        },
-        {
-          label: "Categoría",
-          field: "productCategory",
-          render: (value) => translateCategory(value as string)
-        },
-        {
-          label: "Precio unitario",
-          field: "price",
-          isNumeric: true,
-          render: (value) => `$${value}`
-        },
-        {
-            label: "Acciones",
-            field: "actions",
-            render: (_, row) => (
-                <div className="flex justify-evenly items-center">
-                    <button className="cursor-pointer">
-                        <Pencil className="stroke-primary-foreground"/>
-                    </button>
-                    <button className="">
-                        <Trash2 className="stroke-primary-foreground"/>
-                        </button>
-                </div>
-            )
-        }
-      ];
+  {
+    label: "Nombre",
+    field: "name",
+  },
+  {
+    label: "Descripción",
+    field: "description",
+  },
+  {
+    label: "Categoría",
+    field: "productCategory",
+    render: (value) => translateCategory(value as string),
+  },
+  {
+    label: "Precio unitario",
+    field: "price",
+    isNumeric: true,
+    render: (value) => `$${value}`,
+  },
+  {
+    label: "Acciones",
+    field: "actions",
+    render: (_, row) => (
+      <div className="flex justify-evenly items-center">
+        <button className="cursor-pointer">
+          <Pencil className="stroke-primary-foreground" />
+        </button>
+        <button className="">
+          <Trash2 className="stroke-primary-foreground" />
+        </button>
+      </div>
+    ),
+  },
+];
--- components/product/translations.tsx
+++ components/product/translations.tsx
@@ -1,12 +1,11 @@
-
 export const productEnums = {
-    HAMBURGERS: "Hamburguesas",
-    DRINKS: "Bebidas", 
-    EXTRA: "Extras",
-    POTATOES: "Papas",
-    DESSERTS: "Postres"
+  HAMBURGERS: "Hamburguesas",
+  DRINKS: "Bebidas",
+  EXTRA: "Extras",
+  POTATOES: "Papas",
+  DESSERTS: "Postres",
 };
 
 export function translateCategory(category: string) {
-    return productEnums[category as keyof typeof productEnums] || category;
+  return productEnums[category as keyof typeof productEnums] || category;
 }
