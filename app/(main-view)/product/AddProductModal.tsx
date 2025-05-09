980b58d04aea4d4c7d9e50d5fcd81ecadaa3d028
rosaainz <rosagsainzz@gmail.com>
2025-05-08 18:02:58 -0600

    Add modal add product - name, category, price, description, select subcategory, size(radio button)

--- app/(main-view)/product/AddProductModal.tsx
+++ app/(main-view)/product/AddProductModal.tsx
@@ -15,7 +15,9 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { translateCategory } from "@/components/product/translations";
-
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import IngredientSelector from "@/components/ingredient-selector";
 
 const productSchema = z.object({
   name: z.string().min(1, "El nombre es obligatorio"),
@@ -28,16 +30,16 @@ const productSchema = z.object({
 type ProductFormData = z.infer<typeof productSchema>;
 
 export function AddProductModal({
-    open,
-    onClose,
-    categories,
-    onCategoryChange,
-  }: {
-    open: boolean;
-    onClose: () => void;
-    categories: string[];
-    onCategoryChange: (value: string) => void;
-  }) {
+  open,
+  onClose,
+  categories,
+  onCategoryChange,
+}: {
+  open: boolean;
+  onClose: () => void;
+  categories: string[];
+  onCategoryChange: (value: string) => void;
+}) {
   const {
     register,
     handleSubmit,
@@ -51,79 +53,173 @@ export function AddProductModal({
     console.log("Datos enviados:", data);
     //POST al backend
     reset();
-    onClose(); 
+    onClose();
   };
 
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent className="w-[90vw] max-w-2xl">
+      <DialogContent className="w-[90vw] max-w-2xl sm:max-w-3xl lg:max-w-4xl  bg-primary-foreground border-primary-foreground">
         <DialogHeader>
-          <DialogTitle>Agregar nuevo producto</DialogTitle>
+          <DialogTitle className="text-background text-4xl font-bold tracking-widest border-b border-background pb-2">
+            Añadir nuevo producto
+          </DialogTitle>
         </DialogHeader>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 py-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div>
-                <Input placeholder="Nombre" {...register("name")} />
-                {errors.name && (
-                <p className="text-primary-foreground text-sm">{errors.name.message}</p>
-                )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="w-full">
+              <Label className="text-background text-xl font-bold pb-1">
+                Nombre
+              </Label>
+              <Input
+                className="bg-background focus:border-background"
+                placeholder="Nombre"
+                {...register("name")}
+              />
+              {errors.name && <p className="text-sm">{errors.name.message}</p>}
             </div>
-            <div>
-                <select
-                    className="text-sm px-2 py-2"
-                    {...register("category")}
-                    onChange={(e) => {
-                        onCategoryChange(e.target.value);
-                    }}
-                    >
-                    <option value="">Categorías</option>
-                    {categories.map((category, i) => (
-                        <option key={i} value={category}>
-                        {translateCategory(category)}
-                        </option>
-                    ))}
-                </select>
-                {errors.category && (
+            <div className="w-full">
+              <Label className="text-background text-xl font-bold pb-1">
+                Categoría
+              </Label>
+              <select
+                className="text-sm px-2 py-2 bg-background w-full border border-primary-foreground rounded-md pb-1.5"
+                {...register("category")}
+                onChange={(e) => {
+                  onCategoryChange(e.target.value);
+                }}
+              >
+                <option value="">Categorías</option>
+                {categories.map((category, i) => (
+                  <option key={i} value={category}>
+                    {translateCategory(category)}
+                  </option>
+                ))}
+              </select>
+              {errors.category && (
                 <p className="text-sm">{errors.category.message}</p>
-                )}
+              )}
             </div>
-        </div>
-         
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-           {/* seleccionar tipo */}
+          </div>
 
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {/* Tamaño e ingredientes por defecto */}
-        </div>
-
-        <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 gap-4">
             <div>
-                {/*Cargar Imagen*/ }
-            </div>
-            <div className="grid gap-2">
-                <div>
-                    <Label>Precio</Label>
-                    <Input type="number" step="0.01" {...register("price")} />
-                    {errors.price && (
-                        <p className="text-sm">{errors.price.message}</p>
-                    )}
-                </div>
-                <div>
-                    <Label>Descripción</Label>
-                    <Textarea rows={3} {...register("description")} />
-                </div>
+              <Label className="text-background text-xl font-bold pb-1">
+                Tipo de producto
+              </Label>
             </div>
 
-        </div>      
+            <div className="flex justify-center">
+              <ToggleGroup
+                variant="outline"
+                type="multiple"
+                className="flex flex-wrap gap-2"
+              >
+                <ToggleGroupItem
+                  value="Subcategoria"
+                  aria-label="Subcategoria"
+                  className="px-4 py-1 text-sm sm:text-base truncate border border-background rounded-md data-[state=on]:bg-background data-[state=on]:text-primary-foreground text-background font-bold"
+                >
+                  Hamburguesa de Carne
+                </ToggleGroupItem>
+
+                <ToggleGroupItem
+                  value="Subcategoria"
+                  aria-label="Subcategoria"
+                  className="px-4 py-1 text-sm sm:text-base truncate border border-background rounded-md data-[state=on]:bg-background data-[state=on]:text-primary-foreground text-background font-bold"
+                >
+                  Hamburguesa de Pollo
+                </ToggleGroupItem>
+
+                <ToggleGroupItem
+                  value="Subcategoria"
+                  aria-label="Subcategoria"
+                  className="px-4 py-1 text-sm sm:text-base truncate border border-background rounded-md data-[state=on]:bg-background data-[state=on]:text-primary-foreground text-background font-bold"
+                >
+                  Hamburguesa Vegana
+                </ToggleGroupItem>
+              </ToggleGroup>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div>
+              <Label className="text-background text-xl font-bold pb-2">
+                Tamaño
+              </Label>
+              <div className="flex justify-start text-background font-bold">
+                <RadioGroup defaultValue="comfortable">
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="default" id="r1" />
+                    <Label htmlFor="r1">Sencilla</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="comfortable" id="r2" />
+                    <Label htmlFor="r2">Doble</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="compact" id="r3" />
+                    <Label htmlFor="r3">Triple</Label>
+                  </div>
+                </RadioGroup>
+              </div>
+            </div>
+            <div>
+              <div>
+                <h2 className="text-background text-xl font-bold pb-1">
+                  Ingrediente por defecto
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4">
+            <div>{/*Cargar Imagen*/}</div>
+            <div className="grid gap-2">
+              <div>
+                <Label className="text-background text-xl font-bold pb-1">
+                  Precio
+                </Label>
+                <Input
+                  className="bg-background"
+                  type="number"
+                  step="0.01"
+                  {...register("price")}
+                />
+                {errors.price && (
+                  <p className="text-sm">{errors.price.message}</p>
+                )}
+              </div>
+              <div>
+                <Label className="text-background text-xl font-bold pb-1">
+                  Descripción
+                </Label>
+                <Textarea
+                  className="bg-background"
+                  rows={3}
+                  {...register("description")}
+                />
+              </div>
+            </div>
+          </div>
 
           <DialogFooter>
-            <Button variant="outline" type="button" onClick={onClose}>
-              Cancelar
-            </Button>
-            <Button type="submit">Guardar</Button>
+            <div className="flex gap-4 justify-center sm:justify-end">
+              <Button
+                className="text-primary-foreground text-xl font-bold"
+                variant="outline"
+                type="button"
+                onClick={onClose}
+              >
+                Cancelar
+              </Button>
+              <Button
+                className="text-primary-foreground text-xl font-bold"
+                type="submit"
+              >
+                Guardar
+              </Button>
+            </div>
           </DialogFooter>
         </form>
       </DialogContent>
